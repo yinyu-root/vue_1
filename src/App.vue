9c7e124daf801511347a0a007436d3669eb47be2
yu.yin <yu.yin@datayes.com>
2021-04-16 09:46:14 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!--展示模板-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/zcm.jpg">
+    <Helloa msg="Welcome to My Home"/>
+  </div>
+</template>
+
+<script>
+//导入组件
+//import HelloWorld from './components/HelloWorld.vue'
+import Helloa from "@/components/HelloWorld";
+
+export default {
+  name: 'App',
+  components: {
+    Helloa
+  }
+
+}
+</script>
+
+<!--样式代码-->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For baidu,<br>
+      check out the
+      <a href="https://baidu.com" target="_blank" rel="noopener">baidu</a>.
+    </p>
+    <h2>for baidu</h2>
+    <ul>
+      <li><a href="https://baidu.com" target="_blank" rel="noopener">baidu</a></li>
+      <li><a href="https://baidu.com" target="_blank" rel="noopener">baidu</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
